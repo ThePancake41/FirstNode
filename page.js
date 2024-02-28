bf9aeae07c5f0df53f39bc04c125f85c50a84409
Michael Taylor <88051898+ThePancake41@users.noreply.github.com>
2024-02-28 13:31:59 -0800

    Added event listeners + added click me id and init function

--- page.js
+++ page.js
@@ -6,5 +6,13 @@ const page = {
         childPara.innerHTML = "Hello " + fUsername.value;
         divOutput.appendChild(childPara);
         console.log(fUsername.value);
+    },
+    init: function() {
+        console.log("Page loaded!");
+        const fClickMeButton = document.getElementById("clickMeButton");
+        fClickMeButton.addEventListener("click", page.default());
+        console.log("Done initializing!");
     }
 }
+
+window.addEventListener("load", page.init());
